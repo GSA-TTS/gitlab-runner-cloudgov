7f0a815bff7ab213df812b8a5e0c55343f84b8f8
Paul Hirsch <paul.hirsch@gsa.gov>
2024-04-10 02:27:49 -0500

    Working on adding in GitLab Runner Helper prereqs

--- runner/cf-driver/base.sh
+++ runner/cf-driver/base.sh
@@ -4,3 +4,6 @@
 
 # This name will be long. Hopefully not too long!
 CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID-$CUSTOM_ENV_CI_JOB_ID"
+
+# Use a custom image if provided, else fallback to configured default
+CUSTOM_ENV_CI_JOB_IMAGE="${CUSTOM_ENV_CI_JOB_IMAGE:=$DEFAULT_JOB_IMAGE}"
--- runner/cf-driver/prepare.sh
+++ runner/cf-driver/prepare.sh
@@ -23,20 +23,23 @@ start_container () {
 }
 
 install_dependencies () {
-    # Install Git LFS, git comes pre installed with ubuntu image.
-    cf ssh "$CONTAINER_ID" -c 'curl -s "https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh" | sudo bash'
-    cf ssh "$CONTAINER_ID" -c "apt-get install -y git-lfs"
+    # Build a command to try and install git and git-lfs on common distros.
+    # Of course, RedHat/UBI will need more help to add RPM repos with the correct
+    # version. RedHat support TODO
+    cf ssh "$CONTAINER_ID" -c 'which apk && apk add git && apk add git-lfs; which apt-get && apt-get update && apt-get install -y git git-lfs'
 
-    # Install gitlab-runner binary since we need for cache/artifacts.
-    cf ssh "$CONTAINER_ID" -c 'curl -L --output /usr/local/bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"'
-    cf ssh "$CONTAINER_ID" -c "chmod +x /usr/local/bin/gitlab-runner"
+    # gitlab-runner-helper includes a limited subset of gitlab-runner functionality
+    # plus Git and Git-LFS. https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/index.html
+    #
+    # Install gitlab-runner-helper binary since we need for cache/artifacts. 
+    # TODO: Pin the version and support more arches than X86_64
+    cf ssh "$CONTAINER_ID" -c 'curl -L --output /usr/bin/gitlab-runner-helper "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/latest/binaries/gitlab-runner-helper/gitlab-runner-helper.x86_64"; chmod +x /usr/bin/gitlab-runner-helper'
 }
 
-echo "Starting $CONTAINER_ID with image $CCUSTOM_ENV_CI_JOB_IMAGE"
+echo "Starting $CONTAINER_ID with image $CUSTOM_ENV_CI_JOB_IMAGE"
 start_container
 
-# XXX -I don't like slapping things onto every image - Let's see what breaks without it
-#echo "Installing dependencies into $CONTAINER_ID"
-#install_dependencies
+echo "Installing dependencies into $CONTAINER_ID"
+install_dependencies
 
 echo "$CONTAINER_ID preparation complete"
