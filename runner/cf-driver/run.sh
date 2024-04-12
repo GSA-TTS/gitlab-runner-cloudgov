694f8a99d092e5d0ad6e8df2ac0f378478c26f9b
Paul Hirsch <paul.hirsch@gsa.gov>
2024-04-12 15:05:46 -0500

    Add some surrounding logging to run.sh to be clear on where things croak

--- runner/cf-driver/run.sh
+++ runner/cf-driver/run.sh
@@ -5,9 +5,13 @@
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh
 
+echo "[cf-driver] Using SSH to connect to $CONTAINER_ID and run steps"
+
 cf ssh "$CONTAINER_ID" -c "${1}"
 if [ $? -ne 0 ]; then
     # Exit using the variable, to make the build as failure in GitLab
     # CI.
     exit "$BUILD_FAILURE_EXIT_CODE"
 fi
+
+echo "[cf-driver] Completed SSH session with $CONTAINER_ID to run steps"
