ae3ab841b54cd4b76b32aa2b2b42209e5b9e3d3e
Paul Hirsch <paul.hirsch@gsa.gov>
2024-04-15 01:36:13 -0500

    It is ALIVE!

--- runner/cf-driver/run.sh
+++ runner/cf-driver/run.sh
@@ -7,7 +7,15 @@ source ${currentDir}/base.sh
 
 echo "[cf-driver] Using SSH to connect to $CONTAINER_ID and run steps"
 
-cf ssh "$CONTAINER_ID" -c "${1}"
+if [ -n "$RUNNER_DEBUG" ] && [ "$RUNNER_DEBUG" = "true" ]; then
+    # DANGER: There may be sensitive information in this output.
+    # Generated job logs should be removed after this is used.
+    echo "[cf-driver] RUNNER_DEBUG: About to run the following:\n--------"
+    cat $1
+    echo "--------\n[cf-driver] RUNNER_DEBUG: End command display"
+fi
+
+cf ssh "$CONTAINER_ID" < "${1}"
 if [ $? -ne 0 ]; then
     # Exit using the variable, to make the build as failure in GitLab
     # CI.
