c7fc20b33ec9fb977f1e1a578cd4a60923fd40d8
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2024-11-15 10:51:58 -0500

    feat: parse creds from VCAP_SERVICES env var

--- main_test.go
+++ main_test.go
@@ -5,6 +5,32 @@ import (
 	"testing"
 )
 
+func Test_GetCredentials(t *testing.T) {
+	username := "tjefferson"
+	password := "p4ssw0rdz1"
+
+	json := fmt.Sprintf(
+		`{"cloud-gov-service-account":[{"credentials":{"password":"%s","username":"%s"}}]}`,
+		password,
+		username,
+	)
+
+	t.Setenv("VCAP_SERVICES", json)
+
+	t.Run("Get credentials from VCAP_SERVICES", func(t *testing.T) {
+		credentials, err := GetCredentials()
+		if err != nil {
+			t.Error(err)
+		}
+		if credentials.Username != username {
+			t.Errorf("credentials.Username = %s; want %s", credentials.Username, username)
+		}
+		if credentials.Password != password {
+			t.Errorf("credentials.Password = %s; want %s", credentials.Password, password)
+		}
+	})
+}
+
 func Test_main(t *testing.T) {
 	tests := []struct {
 		name string
