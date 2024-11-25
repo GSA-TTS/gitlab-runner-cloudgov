087439277c66c9cecb803d94c1292f02388d5b72
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2024-11-25 15:48:01 -0500

    fix: move & comment out the CfClient test that snuck in

--- runner/cf-executor/main_test.go
+++ runner/cf-executor/main_test.go
@@ -100,25 +100,31 @@ func TestGetCredentials(t *testing.T) {
 	}
 }
 
+// func TestGetCfClient(t *testing.T) {
+// 	tests := []struct {
+// 		name    string
+// 		want    *client.Client
+// 		wantErr bool
+// 	}{{}}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := GetCfClient()
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("GetCfClient() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("GetCfClient() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
+
 func Test_main(t *testing.T) {
 	t.Skip("Test_main is just for experiments right now")
 
 	tests := []struct{ name string }{{name: "run main"}}
 
-	for _, tt := range tests {
-		fmt.Println(tt.name)
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
-}
-
-func TestGetCfClient(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{name: "scoobie"},
-	}
 	for _, tt := range tests {
 		fmt.Println(tt.name)
 		t.Run(tt.name, func(t *testing.T) {
