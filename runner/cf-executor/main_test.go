02c3cf1f5d16ce1e0012920e0784fa629f87979b
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2024-11-25 09:31:56 -0500

    test: more robust testing of GetCredentials
    - validate failure with bad data
    - uses go-cmp for struct comparison
    - uses table structure to make additional cases easier to add

--- runner/cf-executor/main_test.go
+++ runner/cf-executor/main_test.go
@@ -1,31 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/cloudfoundry/go-cfclient/v3/client"
+	"github.com/google/go-cmp/cmp"
 )
 
-func Test_GetCredentials(t *testing.T) {
-	username := "tjefferson"
-	password := "p4ssw0rdz1"
+var syntaxError *json.SyntaxError
 
-	json := fmt.Sprintf(
-		`{"cloud-gov-service-account":[{"credentials":{"password":"%s","username":"%s"}}]}`,
-		password,
-		username,
-	)
+func getVcapJson(u string, p string) string {
+	return fmt.Sprintf(`{"cloud-gov-service-account":[{"credentials":{"username":"%s","password":"%s"}}]}`, u, p)
+}
 
-	t.Setenv("VCAP_SERVICES", json)
-
-	credentials, err := GetCredentials()
-	if err != nil {
-		t.Error(err)
+func TestGetCredentials(t *testing.T) {
+	tests := []struct {
+		name    string
+		json    string
+		want    *Credentials
+		wantErr interface{}
+	}{
+		{"fails with no JSON", "", nil, &syntaxError},
+		{"fails with malformed JSON", `{"foo": [{"bar": false}}`, nil, &syntaxError},
+		{"pulls credentials from JSON", getVcapJson("aa", "bb"), &Credentials{"aa", "bb"}, nil},
 	}
-	if credentials.Username != username {
-		t.Errorf("credentials.Username = %s; want %s", credentials.Username, username)
-	}
-	if credentials.Password != password {
-		t.Errorf("credentials.Password = %s; want %s", credentials.Password, password)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv("VCAP_SERVICES", tt.json)
+
+			have, err := GetCredentials()
+
+			if (err == nil) != (tt.wantErr == nil) {
+				t.Errorf("GetCfClient() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if err != nil && !errors.As(err, tt.wantErr) {
+				t.Errorf("GetCredentials() bad error type: have %T, want %T", err, tt.wantErr)
+				return
+			}
+
+			if diff := cmp.Diff(tt.want, have); diff != "" {
+				t.Error(diff)
+			}
+		})
 	}
 }
 
