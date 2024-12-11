b2ae777ef72e249b8095fd710c44a065a4670e3e
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2024-12-11 16:03:21 -0500

    chore: delete commented out stub test from main_test.go

--- runner/cf-executor/main_test.go
+++ runner/cf-executor/main_test.go
@@ -5,26 +5,6 @@ import (
 	"testing"
 )
 
-// func TestGetCfClient(t *testing.T) {
-// 	tests := []struct {
-// 		name    string
-// 		want    *client.Client
-// 		wantErr bool
-// 	}{{}}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got, err := GetCfClient()
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("GetCfClient() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("GetCfClient() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
-
 func Test_main(t *testing.T) {
 	t.Skip("Test_main is just for experiments right now")
 
