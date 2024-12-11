1b705bcaa828f35f2b9ef25d3b013caf490ae99e
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2024-12-11 15:56:10 -0500

    test: adding tests for rest of the CG stubs

--- runner/cf-executor/cg/cg_test.go
+++ runner/cf-executor/cg/cg_test.go
@@ -9,28 +9,51 @@ import (
 
 type adapterStub struct {
 	Adapter
+
+	StCreds *Creds
+	StURL   string
+	StApps  []*App
+
+	FailGetApps bool
+	FailConnect bool
 }
 
-func (adapterStub) getApps() (apps []*App, err error) {
-	panic("getApps not implemented") // TODO: Implement
+func (a *adapterStub) getApps() (apps []*App, err error) {
+	if a.FailGetApps {
+		return nil, errors.New("fail")
+	}
+	return a.StApps, nil
 }
 
-func (adapterStub) connect(url string, creds *Creds) (_ error) {
+func (a *adapterStub) connect(url string, creds *Creds) (_ error) {
+	if a.FailConnect {
+		return errors.New("fail")
+	}
+	a.StURL = url
+	a.StCreds = creds
 	return nil
 }
 
-type credIStub struct{}
-
-func (credIStub) getCreds() (*Creds, error) {
-	return &Creds{"", ""}, nil
+type credIStub struct {
+	U    string
+	P    string
+	Fail bool
 }
 
-var (
-	optsStub *Opts = &Opts{CredI: credIStub{}}
-	cgStub   *CG   = &CG{adapterStub{}, optsStub}
-)
+func (c credIStub) getCreds() (*Creds, error) {
+	if c.Fail {
+		return nil, errors.New("fail")
+	}
+	return &Creds{c.U, c.P}, nil
+}
 
 func TestNew(t *testing.T) {
+	optsStub := &Opts{CredI: credIStub{"a", "b", false}}
+	cgStub := &CG{&adapterStub{
+		StURL:   apiRootURLDefault,
+		StCreds: &Creds{"a", "b"},
+	}, optsStub}
+
 	tests := []struct {
 		want    *CG
 		opts    *Opts
@@ -43,20 +66,17 @@ func TestNew(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			have, err := New(adapterStub{}, tt.opts)
-
+			got, err := New(&adapterStub{}, tt.opts)
 			if (err == nil) != (tt.wantErr == nil) {
 				t.Errorf("GetCfClient() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-
 			if err != nil && !errors.As(err, tt.wantErr) {
-				t.Errorf("GetCredentials() bad error type: have %T, want %T", err, tt.wantErr)
+				t.Errorf("GetCredentials() bad error type: got %T, want %T", err, tt.wantErr)
 				return
 			}
-
-			if diff := cmp.Diff(tt.want, have); diff != "" {
-				t.Error(diff)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
@@ -76,7 +96,7 @@ func TestCG_apiRootURL(t *testing.T) {
 			name: "gets default root API URL",
 			want: apiRootURLDefault,
 			fields: fields{
-				Adapter: adapterStub{},
+				Adapter: &adapterStub{},
 				Opts:    &Opts{CredI: credIStub{}},
 			},
 		},
@@ -84,7 +104,7 @@ func TestCG_apiRootURL(t *testing.T) {
 			name: "updates root API URL",
 			want: "foo",
 			fields: fields{
-				Adapter: adapterStub{},
+				Adapter: &adapterStub{},
 				Opts:    &Opts{CredI: credIStub{}, APIRootURL: "foo"},
 			},
 		},
@@ -97,7 +117,7 @@ func TestCG_apiRootURL(t *testing.T) {
 			}
 			got := c.apiRootURL()
 			if diff := cmp.Diff(got, tt.want); diff != "" {
-				t.Error(diff)
+				t.Errorf("mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
@@ -108,14 +128,31 @@ func TestCG_creds(t *testing.T) {
 		Adapter Adapter
 		Opts    *Opts
 	}
+
 	tests := []struct {
 		fields  fields
 		want    *Creds
 		name    string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "returns creds when they already exist",
+			want: &Creds{"a", "b"},
+			fields: fields{
+				Adapter: &adapterStub{},
+				Opts:    &Opts{Creds: &Creds{"a", "b"}},
+			},
+		},
+		{
+			name: "returns creds from getter when not supplied",
+			want: &Creds{"foo", "bar"},
+			fields: fields{
+				Adapter: &adapterStub{},
+				Opts:    &Opts{CredI: credIStub{U: "foo", P: "bar"}},
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &CG{
@@ -128,7 +165,7 @@ func TestCG_creds(t *testing.T) {
 				return
 			}
 			if diff := cmp.Diff(got, tt.want); diff != "" {
-				t.Error(diff)
+				t.Errorf("mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
@@ -139,45 +176,103 @@ func TestCG_Connect(t *testing.T) {
 		Adapter Adapter
 		Opts    *Opts
 	}
+
 	tests := []struct {
 		fields  fields
 		want    *CG
+		cmpGet  func(c *CG) any
 		name    string
+		checks  []string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "fails with creds() err",
+			wantErr: true,
+			fields: fields{
+				Adapter: &adapterStub{},
+				Opts:    &Opts{CredI: &credIStub{Fail: true}},
+			},
+		},
+		{
+			name: "connect sets URL & creds",
+			want: &CG{
+				Adapter: &adapterStub{
+					StURL:   "butter",
+					StCreds: &Creds{Username: "corn", Password: "cob"},
+				},
+			},
+			fields: fields{
+				Adapter: &adapterStub{},
+				Opts: &Opts{
+					APIRootURL: "butter",
+					Creds:      &Creds{Username: "corn", Password: "cob"},
+				},
+			},
+			cmpGet: func(c *CG) any {
+				return c.Adapter
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &CG{
 				Adapter: tt.fields.Adapter,
 				Opts:    tt.fields.Opts,
 			}
+
 			got, err := c.Connect()
+
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CG.Connect() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if diff := cmp.Diff(got, tt.want); diff != "" {
-				t.Error(diff)
+
+			if tt.cmpGet == nil {
+				tt.cmpGet = func(c *CG) any {
+					return c
+				}
+			}
+
+			if diff := cmp.Diff(tt.cmpGet(got), tt.cmpGet(tt.want)); diff != "" {
+				t.Errorf("mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
 }
 
 func TestCG_GetApps(t *testing.T) {
+	testApps := []*App{{Id: "1", Name: "foo"}}
+
 	type fields struct {
 		Adapter Adapter
 		Opts    *Opts
 	}
+
 	tests := []struct {
 		name    string
 		fields  fields
 		want    []*App
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "reports errors",
+			wantErr: true,
+			fields: fields{
+				Adapter: &adapterStub{StApps: testApps, FailGetApps: true},
+				Opts:    &Opts{CredI: &credIStub{}},
+			},
+		},
+		{
+			name: "returns available apps list",
+			want: testApps,
+			fields: fields{
+				Adapter: &adapterStub{StApps: testApps},
+				Opts:    &Opts{CredI: &credIStub{}},
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &CG{
@@ -190,7 +285,7 @@ func TestCG_GetApps(t *testing.T) {
 				return
 			}
 			if diff := cmp.Diff(got, tt.want); diff != "" {
-				t.Error(diff)
+				t.Errorf("mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
