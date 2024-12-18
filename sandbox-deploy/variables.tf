8d09ef610085957aeed30cd776cc335cec60a226
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-12-18 12:09:58 -0500

    Add typing to sandbox-deploy/variables.tf
    Co-authored-by: anna-m-gsa <167908357+anna-m-gsa@users.noreply.github.com>

--- sandbox-deploy/variables.tf
+++ sandbox-deploy/variables.tf
@@ -1,13 +1,22 @@
-variable "cf_user" {}
+variable "cf_user" {
+  type = string
+}
 variable "cf_password" {
+  type = string
   sensitive = true
 }
-variable "cf_space_prefix" {}
+variable "cf_space_prefix" {
+  type = string
+}
 variable "ci_server_token" {
+  type = string
   sensitive = true
 }
-variable "docker_hub_user" {}
+variable "docker_hub_user" {
+  type = string
+}
 variable "docker_hub_token" {
+  type = string
   sensitive = true
 }
 variable "developer_emails" {
