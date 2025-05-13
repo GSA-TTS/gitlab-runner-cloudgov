441f8e1393d389ff932439c27bb270373cd68883
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2025-05-13 11:33:23 -0400

    style: add missing quotes on variable
    Signed-off-by: Zachary J. Rollyson <zachary.rollyson@gsa.gov>

--- runner-manager/cf-driver/base.sh
+++ runner-manager/cf-driver/base.sh
@@ -35,7 +35,7 @@ cf_ssh() {
     container_id="$1"
     command="$2"
     app_guid=$(cf app "$container_id" --guid)
-    SSHPASS=$(cf ssh-code) sshpass -e ssh -p 2222 -T cf:$app_guid/0@ssh.fr.cloud.gov "$command"
+    SSHPASS=$(cf ssh-code) sshpass -e ssh -p 2222 -T "cf:$app_guid/0@ssh.fr.cloud.gov" "$command"
 }
 
 cf_scpr() {
