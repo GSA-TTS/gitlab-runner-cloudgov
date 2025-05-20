7af10984a2f090a34353e71da9313cee3585facd
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-05-20 16:15:25 -0400

    Point glr/sandbox-deploy to cloud-gov-devtools-development

--- sandbox-deploy/main.tf
+++ sandbox-deploy/main.tf
@@ -12,13 +12,14 @@ provider "cloudfoundry" {}
 module "sandbox-runner" {
   source = "../"
 
+  cf_org_name             = "cloud-gov-devtools-development"
   cf_org_manager          = var.cf_org_manager
   cf_space_prefix         = var.cf_space_prefix
   ci_server_token         = var.ci_server_token
   docker_hub_user         = var.docker_hub_user
   docker_hub_token        = var.docker_hub_token
   manager_instances       = 1
-  runner_concurrency      = 4
+  runner_concurrency      = 10
   developer_emails        = var.developer_emails
   worker_disk_size        = var.worker_disk_size
   program_technologies    = var.program_technologies
