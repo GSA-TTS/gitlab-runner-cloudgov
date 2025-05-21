ce12d58ff07e6e79ec65e9f82d432763ee9d11f6
Zachary J. Rollyson <zachary.rollyson@gsa.gov>
2025-05-21 16:09:05 -0400

    feat: use the less coercive git-config for sslCAInfo, add GL's configs
    Signed-off-by: Zachary J. Rollyson <zachary.rollyson@gsa.gov>

--- runner-manager/cf-driver/worker-setup/bundle/glrw-profile.sh
+++ runner-manager/cf-driver/worker-setup/bundle/glrw-profile.sh
@@ -33,8 +33,25 @@ fi
 export PATH="$HOME/bin:/usr/local/bin:/busybox:$PATH"
 export GIT_EXEC_PATH="$HOME/libexec/git-core"
 export GIT_TEMPLATE_DIR="$HOME/share/git-core/templates"
-export GIT_SSL_CAINFO="$SSL_CERT_FILE"
-export GIT_PROXY_SSL_CAINFO="$SSL_CERT_FILE"
+
+if command -v git >/dev/null 2>&1; then
+    git config --global http.sslCAInfo "$SSL_CERT_FILE"
+    git config --global http.proxySSLCAInfo "$SSL_CERT_FILE"
+
+    # These configs are _probably_ meant to be set by GitLab--it tries
+    # to set them, and then immediately deletes them. I think they
+    # probably have a `defer` func that isn't where it shouldn't be.
+    git config --global init.defaultBranch none
+    git config --global fetch.recurseSubmodules false
+    git config --global credential.interactive never
+    git config --global gc.autoDetach false
+    git config --global transfer.bundleURI true
+    if [ -n "$CI_SERVER_TLS_CA_FILE" ]; then
+        git config --global \
+            "http.https://gsa.gitlab-dedicated.us.sslCAInfo" \
+            "$CI_SERVER_TLS_CA_FILE"
+    fi
+fi
 
 for f in "$HOME"/glrw-profile.d/*; do
     [ -e "$f" ] || break
