88e87b5f71478daff8150d32beed2af939005c51
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-07-11 12:15:58 -0400

    Allow replacing sandbox worker space to clear PRESERVEd workers

--- sandbox-deploy/terraform.sh
+++ sandbox-deploy/terraform.sh
@@ -7,11 +7,12 @@ $0: Run terraform commands against a given environment
 
 Usage:
   $0 -h
-  $0 [-f] [-c <TERRAFORM-CMD>] [-- <EXTRA CMD ARGUMENTS>]
+  $0 [-f] [-c <TERRAFORM-CMD>] [-r] [-- <EXTRA CMD ARGUMENTS>]
 
 Options:
 -h: show help and exit
 -f: Force, pass -auto-approve to all invocations of terraform
+-r: Replace worker space, useful to reset after using PRESERVE_WORKER
 -c TERRAFORM-CMD: command to run. Defaults to $cmd
 [<EXTRA CMD ARGUMENTS>]: arguments to pass as-is to terraform
 "
@@ -19,9 +20,10 @@ Options:
 
 force=""
 args_to_shift=0
+declare -a optional_args
 
 set -e
-while getopts ":hfc:" opt; do
+while getopts ":hfrc:" opt; do
   case "$opt" in
     f)
       force="-auto-approve"
@@ -30,6 +32,11 @@ while getopts ":hfc:" opt; do
     c)
       cmd=${OPTARG}
       args_to_shift=$((args_to_shift + 2))
+      ;;
+    r)
+      optional_args+=(-replace=module.sandbox-runner.module.worker_space.cloudfoundry_space.space)
+      optional_args+=(-replace="module.sandbox-runner.module.worker_space.cloudfoundry_security_group_space_bindings.security_group_bindings[\"trusted_local_networks_egress\"]")
+      args_to_shift=$((args_to_shift + 1))
       ;;
     h)
       echo "$usage"
@@ -48,6 +55,6 @@ cf org $dev_org &> /dev/null || cf login -a api.fr-stage.cloud.gov --sso -o $dev
 
 terraform init
 echo "=============================================================================================================="
-echo "= Calling $cmd $force $@ on the sandbox infrastructure"
+echo "= Calling $cmd $force " "${optional_args[@]}" " " "$@" " on the sandbox infrastructure"
 echo "=============================================================================================================="
-terraform "$cmd" $force -compact-warnings "$@"
+terraform "$cmd" $force -compact-warnings "${optional_args[@]}" "$@"
